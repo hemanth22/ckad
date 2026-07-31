ae033401faa1b9c77517fa728c694e0a1aa111e7
sandervanvugt <mail@sandervanvugt.nl>
2026-07-31 08:30:41 +0200

    message

--- minikube-docker-setup.sh
+++ minikube-docker-setup.sh
@@ -4,7 +4,7 @@ ARCH=$(arch)
 
 ### installing Docker
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo apt-get install ca-certificates curl gnupg lsb-release util-linux-extra -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
